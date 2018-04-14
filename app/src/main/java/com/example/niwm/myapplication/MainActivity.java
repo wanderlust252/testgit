88f3c04b61745ace9daf85f864603825e9b0adc7
Mac <mac@Macs-iMac.local>
2018-04-14 15:45:26 +0700

    uupload

--- app/src/main/java/com/example/niwm/myapplication/MainActivity.java
+++ app/src/main/java/com/example/niwm/myapplication/MainActivity.java
@@ -5,13 +5,18 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements   OnClickListener {
     ImageView ani;
     Handler handler;
     int i=1;
@@ -22,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ani = (ImageView) findViewById(R.id.imageview);
+
+
+
+        View layout = (View) findViewById(R.id.frame_layout);
+        layout.setOnClickListener(this);
+
+
+
         handler = new Handler(){
             @Override
             public void handleMessage(Message msg) {
@@ -43,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(iMenu);
                 finish();
             }
-        },3000);
+        },5000);
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -52,5 +65,14 @@ public class MainActivity extends AppCompatActivity {
             }
         },0,50);
 
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        handler.removeCallbacksAndMessages(null);
+        Intent iMenu = new Intent(MainActivity.this,MenuActivity.class);
+        startActivity(iMenu);
+        finish();
     }
 }
